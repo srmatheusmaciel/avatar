f3a83804f917e284a40544b9d045876e55f5cbd7
Matheus Maciel <eumatheusmaciel@gmail.com>
2025-04-06 09:18:39 -0300

    feat:db connection creation completed
    Signed-off-by: Matheus Maciel <eumatheusmaciel@gmail.com>

--- src/main/java/infrastructure/repositories/entities/CustomerProfilePhotos.java
+++ src/main/java/infrastructure/repositories/entities/CustomerProfilePhotos.java
@@ -1,5 +1,9 @@
 package infrastructure.repositories.entities;
 
+import java.util.List;
+
+import domain.models.Customer;
+import domain.models.ProfilePhoto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
@@ -26,6 +30,26 @@ public class CustomerProfilePhotos {
         String customerId;
         @Column(name = "id")
         String Id;
+    }
+
+    public Customer toDomain(){
+        return new Customer(compositeKey.customerId,
+         List.of(new ProfilePhoto(compositeKey.Id,
+                                 originalPhoto,
+                                 generatedPhoto)));
+    }
+
+    public static CustomerProfilePhotos fromDomain(String customerId, ProfilePhoto profilePhoto){
+        var entity = new CustomerProfilePhotos();
+
+        entity.compositeKey = new CompositeKey();
+        entity.compositeKey.customerId = customerId;
+        entity.compositeKey.Id = profilePhoto.id();
+
+        entity.originalPhoto = profilePhoto.originalPhoto();
+        entity.generatedPhoto = profilePhoto.generatedPhoto();
+
+        return entity;
     }
 
 }
